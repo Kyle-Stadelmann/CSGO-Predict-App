7a7037d60e166abff01e3d618c6e6cf2954fbe08
Zachary Tulman <ztulman@ucsd.edu>
2022-10-30 15:37:10 -0700

    made picks and matches available in Voting

--- src/components/Match.tsx
+++ src/components/Match.tsx
@@ -1,27 +1,31 @@
 // TODO: add a submitted status indicator at the right side
-// TODO: make teams show up on opposite ends of div rather than stacked vertically
 import { Match as ApiMatch, Team as ApiTeam } from 'csgo-predict-api';
 import { useState } from 'react';
+import { Picks } from './Pages/Voting';
 import Team from './Team';
 
 type MatchProps = {
     match: ApiMatch;
+    picks: Picks;
+    setPicks: Function;
 }
 
-const Match = ({ match }: MatchProps) => {
+const Match = ({ match, picks, setPicks }: MatchProps) => {
     const team1 = match.team1;
     const team2 = match.team2;
-
+    // theres probably a way to get pickedTeam with picks[match.id] i cant think of how
     const [ pickedTeam, setPickedTeam ] = useState({} as ApiTeam);
-    // load team1 and team2 using setTeamX
 
     function handleTeam1() {
-        setPickedTeam(match.team1);
-        // set picked team in DayPredictions object
+        picks[match.id] = team1.id;
+        setPickedTeam(team1);
+        setPicks({ ...picks });
     }
 
     function handleTeam2() {
-        setPickedTeam(match.team2);
+        picks[match.id] = team2.id;
+        setPickedTeam(team2);
+        setPicks({ ...picks });
     }
 
     return (
@@ -40,6 +44,7 @@ const Match = ({ match }: MatchProps) => {
                 </div>
             </div>
             &nbsp;
+            {/* i want the team name that matches picks[match.id] */}
             {`Currently picked team: ${pickedTeam.name ?? "N/A"}`}
         </div>
     );
