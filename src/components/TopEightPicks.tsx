b13f496d3bcaf0c32837cbba88bf2ee18defcc80
Kyle-Stadelmann <42564445+Kyle-Stadelmann@users.noreply.github.com>
2024-03-15 21:23:25 -0700

    Ran formatter

--- src/components/TopEightPicks.tsx
+++ src/components/TopEightPicks.tsx
@@ -1,25 +1,25 @@
 // this is the user's actual picks for top 8
 // selected teams from TopEightList will come here
 
-import React from 'react';
-import { League } from 'csgo-predict-api';
-import { useDrop } from 'react-dnd';
-import TopEightTeamBucket from './TopEightListBucket';
+import React from "react";
+import { League } from "csgo-predict-api";
+import { useDrop } from "react-dnd";
+import TopEightTeamBucket from "./TopEightListBucket";
 
 const TopEightPicks = ({ teams }: TopEightPicksProps) => {
-    return (
-        <div className="top-eight-picks-wrapper">
-            Top Eight Picks Wrapper
-            <div className="top-eight-picks">
-                Top Eight Picks
-                {teams}
-            </div>
-        </div>
-    );
+	return (
+		<div className="top-eight-picks-wrapper">
+			Top Eight Picks Wrapper
+			<div className="top-eight-picks">
+				Top Eight Picks
+				{teams}
+			</div>
+		</div>
+	);
 };
 
 type TopEightPicksProps = {
-    teams: JSX.Element[],
-}
+	teams: JSX.Element[];
+};
 
 export default TopEightPicks;
