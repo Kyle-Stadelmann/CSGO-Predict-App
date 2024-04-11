0680896b4dc6392c91badd678b445d9569c34fca
unknown <zrtulman@gmail.com>
2024-04-10 19:07:09 -0700

    created base theme and turned it on

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import { isLoggedIn } from "./lib/user-util";
 import ThemeButton from "./components/ThemeButton";
 import { THEME_IMGS } from "./constant";
 import Management from "./components/Pages/Management";
-import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
+import { Theme, ThemeProvider } from "@emotion/react";
+import { CssBaseline, createTheme } from "@mui/material";
 
 function App() {
 	// initialize this as the user's preferred bg image once that's in the db
@@ -18,15 +18,31 @@ function App() {
 	// This line is literally magical. Remove it and the world ends
 	useNavigate();
 
-	const theme = createTheme({
+	// https://mui.com/material-ui/customization/default-theme/
+	const theme: Theme = createTheme({
+		palette: {
+			mode: "dark",
+			background: {
+				paper: "#1d1d1d", // the ui rabbit hole goes deep..
+				default: "#1a1a1a",
+			},
+			text: {
+				primary: "#fff",
+				secondary: "rgba(255, 255, 255, 0.7)",
+				disabled: "rgba(255, 255, 255, 0.5)",
+			},
+			divider: "rgba(255, 255, 255, 0.12)",
+		},
 		typography: {
 			fontSize: 15,
+			fontFamily: ["Verdana", "Arial"].join(","),
 		},
 	});
 
 	return (
-		<div className="app" style={{ backgroundImage: `url(${THEME_IMGS[bgImageIndex]})` }}>
+		<div className="app">
 			<ThemeProvider theme={theme}>
+				<CssBaseline />
 				{/* This stuff will appear on top of every page */}
 
 				{/* This stuff will only appear on its path */}
